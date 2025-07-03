fed5d395e30483c70dc05506dd6a55ab1e653597
Saarthak Johari <saarthak_johari@epam.com>
2025-07-03 13:06:42 +0530

    Added the JSX Expression for interpolation

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+import Child from "./Child";
+function App() {
+  return (
+    <><h1>Hello world!</h1><Child></Child></>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Child.jsx
@@ -0,0 +1,9 @@
+
+function Child() {
+  const userName = "Saarthak Johari"
+  return (
+    <h1>Hello world! from Child Component by {userName}</h1>
+  );
+}
+
+export default Child;
